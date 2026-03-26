28a3211e4b1ceb10d87db372620366a716cb0d28
Erekle Roinishvili <erekleroinishvili@gmail.com>
2026-03-26 11:24:34 +0000

    Socket auto-connect on `LocalStorage` change
    If CF Socket URI is changed in `LocalStorage`,
    the app will automatically try to connect to it
    if an active connection doesn't already exist

--- src/session.ts
+++ src/session.ts
@@ -2,25 +2,37 @@ import { connnectCFSocket } from "./cf-socket";
 import { cfConnectButtonOnClick, hideConfigForm, readCfSocketUriInput, showConfigForm } from "./config-form";
 import { readSocketUriFromSession, writeSocketUriFromSession } from "./local-storage";
 
+let socket: WebSocket;
+
 hideConfigForm();
 cfConnectButtonOnClick(() => {
-    hideConfigForm();
-    connectCfSocket(readCfSocketUriInput());
+    socket = connectCfSocket(readCfSocketUriInput());
 });
 
 let cfSocketUri = readSocketUriFromSession()
 
 if (cfSocketUri) {
     hideConfigForm();
-    connectCfSocket(cfSocketUri);
+    socket = connectCfSocket(cfSocketUri);
 } else {
     showConfigForm();
 }
 
 function connectCfSocket(cfSocketUri: string) {
-    connnectCFSocket(cfSocketUri, {
-        onopen: () => writeSocketUriFromSession(cfSocketUri),
+    return connnectCFSocket(cfSocketUri, {
+        onopen: () => {
+            writeSocketUriFromSession(cfSocketUri)
+            hideConfigForm()
+        },
         onerror: showConfigForm,
         onclose: showConfigForm,
     });
 }
+
+window.addEventListener("storage", () => {
+    let cfSocketUriNew = readSocketUriFromSession()
+    if (cfSocketUriNew && cfSocketUri !== cfSocketUriNew && ! socket?.OPEN) {
+        console.log("Socket in local storage changed")
+        socket = connectCfSocket(cfSocketUriNew);
+    }
+})
